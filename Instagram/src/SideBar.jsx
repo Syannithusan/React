cc3f6446149be7b8b13946d3433d5aa8d9fd7337
syannithusan <syannithusan@gmail.com>
2025-08-19 18:50:09 +0530

    after bi

--- Instagram/src/SideBar.jsx
+++ Instagram/src/SideBar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SideBar() {
   return (
-    <>
-    <div>
+    <div className='m-3'>
+    <div className='d-flex flex-column gap-3'>
       <img className='logo-text' src="src\assets\intagram text.png" alt="" />
       <div><i class="bi bi-house"></i>Home</div>
       <div><i class="bi bi-search-heart"></i>Searh</div>
@@ -14,12 +14,11 @@ function SideBar() {
       <div><i class="bi bi-bag-plus"></i>Create</div>
       <div><i class="bi bi-person-circle"></i>Profile</div>
     </div>
-    <div>
+    <div className='position-fixed  d-flex flex-column gap-3 nb-3'>
       <div><i class="bi bi-threads"></i>Threats</div>
       <div><i class="bi bi-list"></i>More</div>
     </div>
-    </>
+    </div>
   )
 }
-
 export default SideBar
