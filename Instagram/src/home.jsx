552c61c8e2da49cc8af24739932908973e0fdc41
syannithusan <syannithusan@gmail.com>
2025-08-19 08:31:26 +0530

    secound CCommit

--- Instagram/src/home.jsx
+++ Instagram/src/home.jsx
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>ijhuiu</title>
+    <title></title>
 </head>
 <body>
     
