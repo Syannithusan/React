2f733fd3403ce710a725febeb1e40523b96e7dbb
syannithusan <syannithusan@gmail.com>
2025-08-19 17:52:49 +0530

    without color

--- Instagram/src/App.jsx
+++ Instagram/src/App.jsx
@@ -4,9 +4,9 @@ import SideBar from './SideBar'
 function App() {
   return (
     <div className='d-flex vh-100 '>
-      <div className='w-20 bg-danger'><SideBar/></div>
+      <div className='w-20'><SideBar/></div>
       <div className='w-50 bg-info'>feed</div>
-      <div className='w-30 bg-primary'>suggestion</div>
+      <div className='w-30'>suggestion</div>
     </div>
   )
 }
