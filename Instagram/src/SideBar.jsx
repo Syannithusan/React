62a29b5d75c5421e6c44b78abed7e7aa1196a353
syannithusan <syannithusan@gmail.com>
2025-08-19 16:45:34 +0530

    create sideBar menu

--- Instagram/src/SideBar.jsx
+++ Instagram/src/SideBar.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function SideBar() {
   return (
-    <div>SideBar</div>
+    <>
+    <div>
+      <img className='logo-text' src="src\assets\intagram text.png" alt="" />
+      <div>Home</div>
+      <div>Searh</div>
+      <div>Explore</div>
+      <div>Reels</div>
+      <div>Messages</div>
+      <div>Notification</div>
+      <div>Create</div>
+      <div>Profile</div>
+    </div>
+    <div>
+      <div>Threats</div>
+      <div>More</div>
+    </div>
+    </>
   )
 }
 
