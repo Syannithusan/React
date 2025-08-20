f3e8327477b2dea1df74dc41b3cee053545cf957
syannithusan <syannithusan@gmail.com>
2025-08-20 10:02:22 +0530

    after move image to public

--- Instagram/src/Posts.jsx
+++ Instagram/src/Posts.jsx
@@ -1,31 +1,36 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Posts() {
-const [posts, setPosts]=useState([]);
-useEffect(() => {
-  fetch('http://localhost:3000/posts')
-    .then((data) => data.json())
-    .then((data) => setPosts(data))
-    .catch(err => console.log(err))
-}, []);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/posts')
+      .then((data) => data.json())
+      .then((data) => setPosts(data))
+      .catch(err => console.log(err))
+  }, []);
 
   return (
     <div>
-        {posts.length> 0 ? ( 
-            <div>
-                {posts.map((posts)=>
-                <div key={posts.id}>
-                  <img className=' dp rounded-circle' src={posts.user.profile_pic} alt="" />
-                </div>
-                )}
+      {posts.length > 0 ? ( 
+        <div>
+          {posts.map((posts) =>
+            <div key={posts.id}>
+              {/* Display the post image */}
+              <img src={posts.image} alt="post" />
+
+              {/* Optional: display user profile picture */}
+              {/* <img src={posts.user.profile_pic} alt={posts.user.username} /> */}
             </div>
-        ):(
-            <div>
-                Loading Posts
-            </div>
-        )}
+          )}
+        </div>
+      ) : (
+        <div>
+          Loading Posts
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
--- Instagram/src/Stories.jsx
+++ Instagram/src/Stories.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 function Stories() {
   return (
     <div className='story bg-danger'>Stories</div>
