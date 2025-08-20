234f4cae15e74493c7d26cbc53062589af1bc899
syannithusan <syannithusan@gmail.com>
2025-08-20 06:48:48 +0530

    after past photos

--- Instagram/src/Posts.jsx
+++ Instagram/src/Posts.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 function Posts() {
+const [posts, setPosts]=useState([]);
+useEffect(() => {
+  fetch('http://localhost:3000/posts')
+    .then((data) => data.json())
+    .then((data) => setPosts(data))
+    .catch(err => console.log(err))
+}, []);
+
   return (
-    <div>Posts</div>
+    <div>
+        {posts.length> 0 ? ( 
+            <div>
+                {posts.map((posts)=>
+                <div key={posts.id}>
+                  <img className=' dp rounded-circle' src={posts.user.profile_pic} alt="" />
+                </div>
+                )}
+            </div>
+        ):(
+            <div>
+                Loading Posts
+            </div>
+        )}
+    </div>
   )
 }
 
--- Instagram/src/SideBar.jsx
+++ Instagram/src/SideBar.jsx
@@ -5,18 +5,18 @@ function SideBar() {
     <div className='m-3'>
     <div className='d-flex flex-column gap-3'>
       <img className='logo-text' src="src\assets\intagram text.png" alt="" />
-      <div><i class="bi bi-house"></i>Home</div>
-      <div><i class="bi bi-search-heart"></i>Searh</div>
-      <div><i class="bi bi-compass"></i>Explore</div>
-      <div><i class="bi bi-play-btn"></i>Reels</div>
-      <div><i class="bi bi-chat-dots"></i>Messages</div>
-      <div><i class="bi bi-heart"></i>Notification</div>
-      <div><i class="bi bi-bag-plus"></i>Create</div>
-      <div><i class="bi bi-person-circle"></i>Profile</div>
+      <div><i className="bi bi-house"></i>Home</div>
+      <div><i className="bi bi-search-heart"></i>Searh</div>
+      <div><i className="bi bi-compass"></i>Explore</div>
+      <div><i className="bi bi-play-btn"></i>Reels</div>
+      <div><i className="bi bi-chat-dots"></i>Messages</div>
+      <div><i className="bi bi-heart"></i>Notification</div>
+      <div><i className="bi bi-bag-plus"></i>Create</div>
+      <div><i className="bi bi-person-circle"></i>Profile</div>
     </div>
     <div className='position-fixed bottom-0 d-flex flex-column gap-3 mb-3'>
-      <div><i class="bi bi-threads"></i>Threats</div>
-      <div><i class="bi bi-list"></i>More</div>
+      <div><i className="bi bi-threads"></i>Threats</div>
+      <div><i className="bi bi-list"></i>More</div>
     </div>
     </div>
   )
