4c414a1b56ef9fd40255edc59976551dd1307c1c
syannithusan <syannithusan@gmail.com>
2025-08-21 11:33:36 +0530

    before create viewC

--- Instagram/src/Stories.jsx
+++ Instagram/src/Stories.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 function Stories() {
+  const [Stories,setStories]=useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3000/story')
+    .then(data=>data.json())
+    .then(data => setStories(data))
+    .catch(err => console.log(err))
+  },[]);
+
   return (
-    <div className='story bg-danger'>Stories</div>
+    <div className='story d-flex ms-auto'>
+    {Stories.length > 0 ? (
+      Stories.map((story)=>(
+        <div key={story.id} className='ms-1'>
+          <div className='gradient-border'>
+          <img src={story.user.profile_pic} alt="db" className='story-db rounded-circle '/>
+
+          </div>
+          <p className='text-truncate' style={{width:'50px'}}>{story.user.username}</p>
+        </div>
+      ))
+    ):(
+      <p>loading</p>
+    )}
+    </div>
   )
 }
 
